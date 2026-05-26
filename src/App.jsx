87178a8747f3ea86a0aa26c3b1eed819c239abd1
AntonioCappellano <antoniocappellano97@gmail.com>
2026-05-26 18:33:26 +0200

    Milestone 2: Setup Context and Initial fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddTask from "./task/AddTask";
 import TaskList from "./task/TaskList";
 import NavBar from "./components/NavBar";
+import { ContextProvider } from "./contexts/GlobalContext";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add-task" element={<AddTask />} />
-      </Routes>
-    </BrowserRouter>
+    <ContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/add-task" element={<AddTask />} />
+        </Routes>
+      </BrowserRouter>
+    </ContextProvider>
   );
 }
---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+
+function ContextProvider({ children }) {
+  const url = import.meta.env.VITE_API_URL;
+  const [taskList, setTaskList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${url}/tasks`)
+      .then((r) => r.json())
+      .then((data) => setTaskList(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    console.log(taskList);
+  }, [taskList]);
+
+  const value = { taskList, url, setTaskList };
+
+  return (
+    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+  );
+}
+
+function useTaskList() {
+  const context = useContext(GlobalContext);
+  return context;
+}
+
+export { ContextProvider, useTaskList };
